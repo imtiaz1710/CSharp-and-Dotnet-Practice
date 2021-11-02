563069949858bfbdba560e34607abde23bd05317
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2021-11-02 10:52:27 +0600

    added

--- ADONetPractice/ADONetPractice/Program.cs
+++ ADONetPractice/ADONetPractice/Program.cs
@@ -9,18 +9,20 @@ namespace ADONetPractice
 {
     class Program
     {
-        private const string connectionString= "Server=DESKTOP-JO34CC1\\SQLEXPRESS;Database=Practice;User Id=demo;Password=123456;";
+        private const string connectionString= 
+            "Server=DESKTOP-JO34CC1\\SQLEXPRESS;Database=Practice;User Id=demo;Password=123456;";
+
         static void Main(string[] args)
         {
             var connection = new SqlConnection(connectionString);
-            DataTableExample(connection);
-            //InsertOperationRun();
+            InsertOperationRun();
         }
 
         public static void InsertOperationRun()
         {
             Console.WriteLine("Insert the number of rows you want to insert in table: ");
             int numberOfRows = int.Parse(Console.ReadLine());
+
             for (int i = 0; i < numberOfRows; i++)
             {
                 Console.WriteLine("Insert Name and age in separate line:");
@@ -28,6 +30,7 @@ namespace ADONetPractice
                 var name = Console.ReadLine();
                 var age = int.Parse(Console.ReadLine());
                 var connection = new SqlConnection(connectionString);
+
                 InsertExecution(insert, connection, new Dictionary<string, object>
                 {
                     { "@name",name },
@@ -35,6 +38,7 @@ namespace ADONetPractice
                 });
             }
         }
+
         public static void InsertExecution(string insert,SqlConnection connection,
             IDictionary<string,object> keyValuePairs)
         {
@@ -49,37 +53,6 @@ namespace ADONetPractice
             connection.Close();
             command.Dispose();
             connection.Dispose();
-        }
-        public static void DataTableExample(SqlConnection connection)
-        {
-            var dataTable = new DataTable()
-            {
-                TableName = "MyTable"
-            };
-            dataTable.Columns.Add("Name",typeof(string));
-            dataTable.Columns.Add("age", typeof(int));
-
-            var dataRow = dataTable.NewRow();
-            dataRow["Name"] = "Muhit";
-            dataRow["age"] = 3;
-
-            dataTable.Rows.Add(dataRow);
-
-            dataRow = dataTable.NewRow();
-            dataRow["Name"] = "Mukit";
-            dataRow["age"] = 7;
-
-            dataTable.Rows.Add(dataRow);
-
-            dataTable.WriteXml(@"G:\DevskillDotNetCourse\aspnet-b5-personal\ADONetPractice\data.xml");
-            var json = JsonConvert.SerializeObject(dataTable);
-            File.WriteAllText(@"G:\DevskillDotNetCourse\aspnet-b5-personal\ADONetPractice\Data.json",json);
-           
-           /* SqlDataAdapter adapter = new SqlDataAdapter("select * from student", connection);
-            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
-            builder.GetInsertCommand();
-            adapter.Update(dataTable);*/
-
         }
     }
 }
---
+++ practice/ConverterXmlToJsonByDataTable/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ConverterXmlToJsonByDataTable
+{
+    class Program
+    {
+        private const string connectionString =
+            "Server=DESKTOP-JO34CC1\\SQLEXPRESS;Database=Practice;User Id=demo;Password=123456;";
+
+        static void Main(string[] args)
+        {
+            var connection = new SqlConnection(connectionString);
+            DataTableExample(connection);
+        }
+
+        public static void DataTableExample(SqlConnection connection)
+        {
+            var dataTable = new DataTable()
+            {
+                TableName = "MyTable"
+            };
+
+            dataTable.Columns.Add("Name", typeof(string));
+            dataTable.Columns.Add("age", typeof(int));
+
+            var dataRow = dataTable.NewRow();
+            dataRow["Name"] = "Muhit";
+            dataRow["age"] = 3;
+
+            dataTable.Rows.Add(dataRow);
+
+            dataRow = dataTable.NewRow();
+            dataRow["Name"] = "Mukit";
+            dataRow["age"] = 7;
+
+            dataTable.Rows.Add(dataRow);
+
+            dataTable.WriteXml(@"G:\DevskillDotNetCourse\aspnet-b5-personal\ADONetPractice\data.xml");
+            var json = JsonConvert.SerializeObject(dataTable);
+            File.WriteAllText(@"G:\DevskillDotNetCourse\aspnet-b5-personal\ADONetPractice\Data.json", json);
+
+             /*SqlDataAdapter adapter = new SqlDataAdapter("select * from student", connection);
+             SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+             builder.GetInsertCommand();
+             adapter.Update(dataTable);*/
+        }
+    }
+}
